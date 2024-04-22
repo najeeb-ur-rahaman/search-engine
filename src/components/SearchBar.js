e3fb1552e5bbda0b318105920813e1b199ebc9f3
najeebshaik08@gmail.com <najeebshaik08@gmail.com>
2024-04-22 20:48:40 +0800

    assets

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,11 +5,15 @@ import { Link } from 'react-router-dom';
 import SignupPage from './SingnupPage'
 import '../Form.css'; // Import CSS file for styling
 import Home from './Home';
+import logo from '../../src/Assests/logout-1.jpg'
+import axios from 'axios';
 const SearchBar = ({isLoggedIn}) => {
 
     const [data,setData]=useState([]);
     const inputElement = useRef();
     const [showButton,setShowButton]=useState(true);
+
+    const input1= useState('2024');
 //  const handleSubmit= async () => {
 //     alert("hello")
 //   console.log(inputElement.current)
@@ -38,13 +42,126 @@ const center1= {
   backgroundColor:"blue"
 }
 
-    const getData = async () => {
-        console.log("hello" + inputElement.current?.value);
-        const res = await fetch(`http://localhost:8080/search/${inputElement.current?.value}`);
-        const data = await res.json();
+    // const getData = async () => {
+    //   const req1={
+    //        keyword:inputElement.current?.value,
+    //        date1:''
+    //   }
+    //     console.log("hello" + inputElement.current?.value);
+    //    // const res = await fetch(`http://localhost:8080/search/${inputElement.current?.value}/''`);
+
+    //    const res = await axios.get('http://localhost:8080/search',{
+    //    params: req1
+    // })
+    //   const data = res.json();
+    //     setData(data);
+    //     setShowButton(false)
+  
+    //     // const data = await res.json();
+    //     // setData(data);
+    //     // setShowButton(false)
+    //   };
+    //   // const getData1 = async () => {
+    //   //   console.log("hello1=====" + inputElement.current?.value);
+    //   //   const res = await fetch(`http://localhost:8080/search/${inputElement.current?.value}/2024`);
+    //   //   const data = await res.json();
+    //   //   setData(data);
+    //   //   setShowButton(false)
+    //   // };
+    const headers = {
+      'Content-Type': 'application/json'
+    }
+    const searchRequest={
+      name:inputElement.current?.value,
+      date:''
+ }
+
+ const req1={
+  name:inputElement.current?.value,
+  date:''
+}
+      const getData =  async () => {
+
+        console.log(req1);
+        
+    const response =  await fetch("http://localhost:8080/search",{
+            name:'react',
+            date1:''
+         },{
+         headers:headers
+         }
+         );
+      const data=response.json();
+
+  
+    // Handle successful response
+    setData(data);
+    setShowButton(false)
+ 
+      };
+
+
+
+
+   const getData2=   async () => {
+        const rawResponse = await fetch('http://localhost:8080/search', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name:inputElement.current?.value,
+            date1:''
+         })
+        });
+        const data = await rawResponse.json();
+      
         setData(data);
         setShowButton(false)
       };
+      const getData3=   async (p) => {
+        console.log("getData3")
+        const rawResponse = await fetch('http://localhost:8080/search', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name:inputElement.current?.value,
+            date1:p
+         })
+        });
+        const data = await rawResponse.json();
+      
+        setData(data);
+        setShowButton(false)
+      };
+
+
+     
+
+      const getData1 = async () => {
+        const req1={
+             keyword:inputElement.current?.value,
+             date1:2024
+        }
+          console.log("hello1" + inputElement.current?.value);
+         // const res = await fetch(`http://localhost:8080/search/${inputElement.current?.value}/''`);
+  
+         const res = await fetch('http://localhost:8080/search/',{
+         params: req1
+      }).then(function(response){
+        const data = response.json();
+          setData(data);
+          setShowButton(false)
+      })
+          // const data = await res.json();
+          // setData(data);
+          // setShowButton(false)
+        };
+
     return(
    <div className="auth-container1">
 
@@ -55,22 +172,31 @@ const center1= {
         </div> 
       }
     </div> */}
+
+     <div style={{position:"absolute", left:"10px",top:"100px",color:"blue"}}>
+      {data &&
+        <div>
+        <p onClick={()=>getData3(2024)} value={'2024'}>since 2024</p>
+        <p onClick={()=>getData3(2023)} value={'2023'}>since 2023</p>
+        <p onClick={()=>getData3(2022)} value={'2022'}>since 2022</p>
+        </div>}
+    </div> 
    
           <div style={{position:"absolute", right:"100px",top:"100px"}}>
-         <Link to="/" element={<Home/>}>Logout</Link>
+         <Link to="/" element={<Home/>}><img src={logo} alt={logo} width="50" height="50"></img></Link>
          </div>
      {showButton? 
      <div style={center}> 
       <input  type="text" ref={inputElement} size="50"/>
         
-      <button onClick={getData}>Search</button>
+      <button onClick={getData2}>Search</button>
       </div>
 
     :
       <div>
         <input  type="text" ref={inputElement} size="50"/>
-        <button onClick={getData}>Search</button>
-      <SearchData data={data}/>
+        <button onClick={getData2}>Search</button>
+      <SearchData data={data} />
       </div>
 }
         </div>)
--- src/components/SearchData.js
+++ src/components/SearchData.js
@@ -9,7 +9,7 @@ const SearchData= ({data}) => {
         titleText: {
           fontSize: 20,
           fontWeight: 'bold',
-          color:'green'
+          color:'purple'
         },
       };
     return(
@@ -24,10 +24,11 @@ const SearchData= ({data}) => {
     {
         data.map((e)=> (
         <div >
-            <p style={styles.titleText}>{e.title}</p>
-            <p style={{color:'green'}}>{e?.publication_info?.summary}</p>
-            <p style={styles.titleText}>{e.snippet}</p>
-       <a href={e.url} target="_blank">{e.url}</a>
+            
+            <p style={{color:'green'}}>{e?.title}</p>
+             <p style={{color:'green'}}>{e?.publication_info?.summary}</p>
+            <p style={styles.titleText}>{e?.snippet}</p>
+       <a href={e.url} target="_blank">{e?.url}</a> 
        <br/>
        <br/>
        </div>
